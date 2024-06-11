c13275a46f5f5948be74e952976f288927a43d2c
Ankit <ankittiwarieng@gmail.com>
2024-06-11 19:27:23 +0545

    first commit

--- springboot-app/src/main/java/com/project/springboot_app/Controller/FetchingController.java
+++ springboot-app/src/main/java/com/project/springboot_app/Controller/FetchingController.java
@@ -1,6 +1,8 @@
 package com.project.springboot_app.Controller;
 
 import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +18,12 @@ import com.project.springboot_app.Model.Recipe;
 import com.project.springboot_app.Model.RecipeDetails;
 import com.project.springboot_app.service.FetchingService;
 import com.project.springboot_app.service.RecipeService;
+
+
+import jakarta.servlet.http.HttpSession;
+
 //import com.project.springboot_app.service.RecipeService;
+//import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -30,18 +37,45 @@ public class FetchingController {
      RecipeDetails recipeDetails;
 
 
-
+   
     @GetMapping("/login")
     public String index(){
         return"index";
     }
+    @GetMapping("/logout")
+        public String logout(HttpSession session ,Model model){
+            session.removeAttribute("is_authenticated");
+            List<RecipeDetails> allrecipe=recipeService.getAllRecipe();
+      Random randomRecipe = new Random();
+      
+      List<RecipeDetails> randomrecipe = new ArrayList<>();
+      
+      int i = randomRecipe.nextInt(allrecipe.size());
+      int j = randomRecipe.nextInt(allrecipe.size());
+      while(j == i){
+        j = randomRecipe.nextInt(allrecipe.size());
+      }
+      int k = randomRecipe.nextInt(allrecipe.size());
+      while(k == i | k == j){
+        k = randomRecipe.nextInt(allrecipe.size());
+      }
+      randomrecipe.add(allrecipe.get(i));
+      randomrecipe.add(allrecipe.get(j));
+      randomrecipe.add(allrecipe.get(k));
+
+      
+      
+      model.addAttribute("recipeItems", randomrecipe);
+            return "home";
+        }
+    
    
 
 //    public Recipe saveRecipe(@RequestParam Recipe recipe){
 //       return recipeService.create(recipe);
 //     }
 @PostMapping("/admin")
-public String authenticateUser(@ModelAttribute("loginForm") Recipe loginForm,Model model) {
+public String authenticateUser(@ModelAttribute("loginForm") Recipe loginForm,Model model ,HttpSession session) {
     boolean authenticatedUser = fetchingService.authenticate(loginForm.getId(),loginForm.getUsername(), loginForm.getPassword());
    // List<RecipeDetails>=  recipeDetailsRepository.findAll();
     
@@ -50,10 +84,14 @@ public String authenticateUser(@ModelAttribute("loginForm") Recipe loginForm,Mod
     System.out.println("value of all recipe is "+allrecipe);
     
     if (authenticatedUser) {
+
+        session.setAttribute("is_authenticated", true);
+           
+
     model.addAttribute("recipeItems",allrecipe);
-        return "home"; //admin page
+        return "recipelist"; //admin page
     } else {
-       // model.addAttribute("error", "Invalid username or password");
+        session.setAttribute("is_authenticated", false);
         return "unsucess"; 
     }
 }
